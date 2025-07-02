d5e932b015c43a429f86689d17cd9ee3bc44e040
nicolexrs <nicandason.xx@gmail.com>
2025-07-02 23:27:48 +0100

    Update TechStack.jsx

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -37,12 +37,12 @@ const TechStack = () => {
 
   return (
     <div id="skills" className="flex items-center justify-center section-padding">
-      <div className="w-full h-full md:px-10 px-5 flex items-center justify-center">
+      <div className="w-full h-full md:px-10 px-5 ">
         <TitleHeader
           title="How I Can Contribute & My Key Skills"
           sub="🤝 What I Bring to the Table"
         />
-        <div className=" flex items-center justify-center flex-col">
+        <div className="tech-grids">
           {/* Loop through the techStackIcons array and create a component for each item. 
               The key is set to the name of the tech stack icon, and the classnames are set to 
               card-border, tech-card, overflow-hidden, and group. The xl:rounded-full and rounded-lg 
